08fef7f7c3b2d55fff08d885dbe2194f6f293f3c
marcuspresa <43669327+marcuspresa@users.noreply.github.com>
2021-02-24 17:36:55 +0100

    Add files via upload
    Speed Limit - https://open.kattis.com/problems/speedlimit

---
+++ Main.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<Integer> finalSpeed = new ArrayList<Integer>();
+        int value = 0;
+        while ((value = scanner.nextInt()) != -1) {
+               int count = value;
+               int previousHour = 0;
+               int totalmiles = 0;
+               int index = 0;
+               while ((index++) < count) {
+                   System.out.println(count);
+                   int speed = scanner.nextInt();
+                   int currentHour = scanner.nextInt();
+                   totalmiles += speed * (currentHour - previousHour);
+                   previousHour = currentHour;
+                }
+                finalSpeed.add(totalmiles);
+            }
+            for (int miles : finalSpeed) {
+                System.out.println(miles + " miles");
+            }
+        }
+
+
+    }
+
+
