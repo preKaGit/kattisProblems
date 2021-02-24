5cd8c0c9a8bec720bc9b16fcb9bde9928b3659f4
marcuspresa <43669327+marcuspresa@users.noreply.github.com>
2021-02-24 17:37:54 +0100

    Update and rename Main.java to SpeedLimit.java

--- SpeedLimit.java
+++ SpeedLimit.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Main {
+public class SpeedLimit {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         List<Integer> finalSpeed = new ArrayList<Integer>();
